965627bfe161dd90739a7fb687bbdfd0c0b6f28c
y4cer <a.buchnev@innopolis.university>
2024-04-12 22:27:24 +0300

    r-mol: initial commit

---
+++ main.py
@@ -0,0 +1,116 @@
+from fastapi import FastAPI, Request, Form
+from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse, RedirectResponse
+from fastapi import UploadFile, File
+import aiofiles
+import re
+import shutil
+
+app = FastAPI()
+
+templates = Jinja2Templates(directory="templates/")
+
+
+class User:
+    def __init__(self):
+        self.password = None
+        self.username = None
+
+    def init(self, username: str, password: str):
+        self.username = username
+        self.password = password
+
+
+def check_password(password: str):
+    # Password should at least be 8 characters long, have a number, an uppercase and a lowercase letter,
+    # and a special character
+    return len(password) >= 8 and re.search(r'\d', password) is not None and re.search(r'[A-Z]',
+                                                                                       password) is not None and re.search(
+        r'[a-z]', password) is not None and re.search(r'\W', password) is not None
+
+
+@app.get("/signup", response_class=HTMLResponse)
+async def signup_form(request: Request):
+    return templates.TemplateResponse("auth/signup.html", {"request": request})
+
+
+@app.post("/signup")
+async def signup(request: Request, username: str = Form(...), password: str = Form(...)):
+    async with aiofiles.open('users.txt', mode='r') as file:
+        users = [line.split(":")[0] for line in await file.readlines()]
+    if username in users:
+        return templates.TemplateResponse('auth/signup.html', {"request": request, "error": "Username already exists."})
+    elif not check_password(password):
+        return templates.TemplateResponse('auth/signup.html', {"request": request, "error": "Weak password. Password "
+                                                                                            "should be at least 8 "
+                                                                                            "characters, "
+                                                                                            "include uppercase, "
+                                                                                            "lowercase, numbers, "
+                                                                                            "and a special character."})
+    else:
+        async with aiofiles.open('users.txt', mode='a') as file:
+            await file.write(f'{username}:{password}\n')
+        return RedirectResponse(url='/internal', status_code=303)
+
+
+@app.get("/signin", response_class=HTMLResponse)
+async def login_form(request: Request):
+    return templates.TemplateResponse("auth/login.html", {"request": request})
+
+
+@app.post("/signin")
+async def login(request: Request, username: str = Form(...), password: str = Form(...)):
+    async with aiofiles.open('users.txt', mode='r') as file:
+        users = [line.strip().split(":") for line in await file.readlines()]
+    for user in users:
+        if user[0] == username and user[1] == password:
+            return RedirectResponse(url='/internal', status_code=303)
+    return templates.TemplateResponse('auth/login.html',
+                                      {"request": request, "error": "Incorrect username or password."})
+
+
+@app.get("/internal", response_class=HTMLResponse)
+async def internal(request: Request):
+    return templates.TemplateResponse("internal/internal.html", {"request": request})
+
+
+@app.get("/logout")
+async def logout():
+    return RedirectResponse(url='/signin', status_code=303)
+
+
+def get_user_data():
+    return "rmol", "user_photos/rmol.png"
+
+
+@app.get("/account", response_class=HTMLResponse)
+async def account(request: Request):
+    username, user_photo_path = get_user_data()
+    return templates.TemplateResponse(
+        "account/account.html", {"request": request, "username": username, "photo": user_photo_path or None})
+
+
+@app.post("/update_account")
+async def update_account(username: str = Form(...), photo: UploadFile = File(...)):
+    async with aiofiles.open('users_photos.txt', mode='r') as file:
+        users = [(user_name, user_photo.strip())
+                 for user_name, user_photo in (line.split(":") for line in await file.readlines())]
+    for i, user in enumerate(users):
+        if user[0] == username:
+            user_photo_path = f"user_photos/{photo.filename}"
+            with open(user_photo_path, 'wb') as buffer:
+                shutil.copyfileobj(photo.file, buffer)
+            users[i] = (username, user_photo_path)
+            break
+    else:
+        return {
+            "status": False,
+            "detail": "User does not exist"
+        }
+    async with aiofiles.open('users_photos.txt', mode='w') as file:
+        for user_name, user_photo in users:
+            await file.write(f'{user_name}:{user_photo}\n')
+    return {
+        "status": True,
+        "detail": "User account has been updated"
+    }
