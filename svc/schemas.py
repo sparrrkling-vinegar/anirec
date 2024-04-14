a277f10fcc2347f306d6a9c446d684df1d790240
y4cer <a.buchnev@innopolis.university>
2024-04-14 11:01:28 +0300

    add anime by name and random anime functionality

---
+++ svc/schemas.py
@@ -0,0 +1,12 @@
+import pydantic
+
+class Anime(pydantic.BaseModel):
+    title: str
+    mal_id: int
+    main_picture: str
+    popularity: int
+    synopsis: str
+    rating: str
+    genre_list: list[str]
+    episodes: int
+    duration: int
