3b1ce12080c392517f57f7e27333b6f678ebc8a4
y4cer <a.buchnev@innopolis.university>
2024-04-25 07:08:14 +0300

    fix jwt

--- auth/security.py
+++ auth/security.py
@@ -25,69 +25,24 @@ class AuthHandler():
     oauth2_scheme = fastapi_security.OAuth2PasswordBearer(tokenUrl="token")
     user_svc: user_service.UserService
 
-    def __init__(self) -> None:
+    def __init__(self, secret_key: bytes) -> None:
         # 16 bytes is enough for secure signing
         user_repo = user_repository.UserRepository()
         self.user_svc = user_service.UserService(user_repo)
-        self.SECRET_KEY = os.urandom(16)
+        self.SECRET_KEY = secret_key
     
     # For internal (this package) usage only
     # Used to create jwts
-    def _create_access_token(self,
+    def create_access_token(self,
         username: str, 
         expires_delta_minutes: datetime.timedelta = ACCESS_TOKEN_EXPIRE_MINUTES
     ) -> str:
         # We call this method if and only if the user is already registered, i.e.
         # we cannot issue the token to the unknown user
 
-        # TODO: probably we do not need this comparison, who knows
-        if username != self.user_svc.get(username):
-            raise user_service.UserDoesNotExist()
-            
         expire = datetime.datetime.now(self.DEFAULT_TIMEZONE) + expires_delta_minutes
 
         to_encode = {"sub": username, "exp": expire}
         encoded_jwt = jwt.encode(to_encode, self.SECRET_KEY, algorithm=self.ALGORITHM)
         return encoded_jwt
 
-    # Use this funcion to exchange your token to your username
-    def get_current_user(self, token: typing.Annotated[str, fastapi.Depends(oauth2_scheme)]):
-        credentials_exception = fastapi.HTTPException(
-            status_code=401,
-            detail="Could not validate credentials",
-            headers={"WWW-Authenticate": "Bearer"},
-        )
-        try:
-            payload = jwt.decode(token, self.SECRET_KEY, algorithm=self.ALGORITHM)
-            username: str = payload.get("sub")
-            if username is None:
-                raise credentials_exception
-        except jwt.PyJWTError:
-            raise credentials_exception
-        
-        user = self.user_svc.get(username)
-        
-        if user is None:
-            raise credentials_exception
-
-        return user
-
-    # Use this function when you want to get new token and you are already registerd
-    def login_for_access_token(
-        self,
-        form_data: typing.Annotated[fastapi_security.OAuth2PasswordRequestForm, fastapi.Depends()],
-    ) -> Token:
-        user = self.user_svc.login(form_data.username, form_data.password)
-        if not user:
-            raise fastapi.HTTPException(
-                status_code=401,
-                detail="Incorrect username or password",
-                headers={"WWW-Authenticate": "Bearer"},
-            )
-
-        access_token = self._create_access_token(
-            username=user.username,
-            expires_delta_minutes=self.ACCESS_TOKEN_EXPIRE_MINUTES,
-        )
-
-        return Token(access_token=access_token, token_type="bearer")
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from fastapi import FastAPI, Request, Form
-from fastapi import UploadFile, File
+from fastapi import FastAPI, Request, Form, HTTPException
+from fastapi import UploadFile, File, Depends
 from fastapi.responses import HTMLResponse, RedirectResponse
 from fastapi.templating import Jinja2Templates
 from typing import List
@@ -7,17 +7,27 @@ import re
 from fastapi.staticfiles import StaticFiles
 import auth.schemas as auth_schemas
 from pydantic import BaseModel
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 
 from services.user_service import UserServiceFactory, UserDoesNotExist, WrongPassword, UserAlreadyExists, WeakPassword
 from svc.anime import get_anime_by_name, get_random_anime
 from svc import schemas as svc_schemas
 import schemas
+from auth.security import AuthHandler, Token
+import jwt
+from typing import Annotated
+import datetime
 
 app = FastAPI()
 app.mount("/user_photos", StaticFiles(directory="user_photos"), name="user_photos")
 
 templates = Jinja2Templates(directory="templates/")
 
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="signin")
+
+ACCESS_TOKEN_EXPIRE_MINUTES = datetime.timedelta(60)
+secret_key = b"111111111111111111111111"
+auth_handler = AuthHandler(secret_key)
 
 @app.get("/", response_class=HTMLResponse)
 def root(request: Request):
@@ -60,32 +70,55 @@ async def signup(request: Request, username: str = Form(...), password: str = Fo
             "and a special character.",
             400
         )
-    return RedirectResponse(url='/internal', status_code=303)
+
+    token = auth_handler.create_access_token(username)
+    resp = RedirectResponse(url='/internal', status_code=303)
+    resp.set_cookie(key="access_token", value=token)
+    return resp
 
 
 @app.get("/signin", response_class=HTMLResponse)
 async def login_form(request: Request):
     return templates.TemplateResponse("auth/login.html", {"request": request})
 
-
-@app.post("/signin")
-async def login(request: Request, username: str = Form(...), password: str = Form(...)):
-    user_service = UserServiceFactory.make()
-
-    def failure_response_factory(text: str, status_code: int):
-        return templates.TemplateResponse(
-            'auth/login.html',
-            {"request": request, "error": text},
-            status_code=status_code
-        )
-
+# Use this funcion to exchange your token to your username
+def get_current_user(self, token: Annotated[str, Depends(oauth2_scheme)]):
+    credentials_exception = HTTPException(
+        status_code=401,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
     try:
-        user_service.login(username=username, password=password)
-    except UserDoesNotExist:
-        return failure_response_factory("User does not exist.", 404)
-    except WrongPassword:
-        return failure_response_factory("Incorrect password.", 403)
-    return RedirectResponse(url='/internal', status_code=303)
+        payload = jwt.decode(token, self.SECRET_KEY, algorithm=self.ALGORITHM)
+        username: str = payload.get("sub")
+        if username is None:
+            raise credentials_exception
+    except jwt.PyJWTError:
+        raise credentials_exception
+    
+    user = self.user_svc.get(username)
+    
+    if user is None:
+        raise credentials_exception
+
+    return user
+
+
+# Use this function when you want to get new token and you are already registerd
+@app.post("/signin")
+def check_token(form_data: Annotated[OAuth2PasswordRequestForm, Depends()]):
+    user_service = UserServiceFactory.make()
+    try:
+        print(form_data.username, form_data.password)
+
+        user = user_service.login(form_data.username, form_data.password)
+    except UserDoesNotExist or WrongPassword:
+        raise HTTPException(status_code=400, detail="Incorrect username or password")
+
+    token = auth_handler.create_access_token(user.username)
+    resp = RedirectResponse(url='/internal', status_code=303)
+    resp.set_cookie(key="access_token", value=token)
+    return resp
 
 
 @app.get("/internal", response_class=HTMLResponse)
@@ -138,7 +171,7 @@ async def update_account(user: str, username: str = Form(...), photo: UploadFile
 
 
 @app.get("/recommendation", response_class=HTMLResponse)
-async def get_page(request: Request):
+async def get_recommendation(request: Request):
     return templates.TemplateResponse("internal/recommendation.html", {"request": request})
 
 
@@ -165,10 +198,12 @@ texts = [f"Text {i}" for i in range(10)]
 
 
 @app.get("/search_page", response_class=HTMLResponse)
-async def get_page(request: Request):
+async def get_search_page(request: Request):
     return templates.TemplateResponse("internal/search.html", {"request": request})
 
 
 @app.post("/search")
 async def search(request_data: Search) -> List[svc_schemas.Anime]:
     return get_anime_by_name(request_data.search)
+
+
